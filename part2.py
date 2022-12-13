8b62de31461ac844afbd308f0b19b04a187ec35a
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2022-12-13 08:17:36 +0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,1 +1,53 @@
-.
+def sentence_splitter(text):
+    stri = open(text)
+    s = stri.read()
+    a = s.split()
+
+    for words in a:
+        if words.endswith("?") or words.endswith("!"):
+            print(words, end = "\n")
+            continue
+        else:
+            if (words.startswith("Mr") and words.endswith(".")) or (words.startswith("i.e") and words.endswith(".")) or (words.startswith("Jr") and words.endswith(".")) or (words == "..."):
+                print(words, end = " ")
+                continue
+            elif (words.endswith(".")):
+                print(words, end = "\n")
+                continue
+            elif (words == "isnt."):
+                print(words)
+                break
+            else:
+                print(words, end = " ")
+                continue
+
+    
+
+sentence_splitter('something.txt')
+
+def sentence_splitter(text):
+    stri = open(text)
+    s = stri.read()
+    a = s.split()
+
+    for words in a:
+        if words.endswith("?") or words.endswith("!"):
+            print(words, end = "\n")
+            continue
+        else:
+            if (words.startswith("Mr") and words.endswith(".")) or (words.startswith("i.e") and words.endswith(".")) or (words.startswith("Jr") and words.endswith(".")) or (words == "..."):
+                print(words, end = " ")
+                continue
+            elif (words.endswith(".")):
+                print(words, end = "\n")
+                continue
+            elif (words == "isnt."):
+                print(words)
+                break
+            else:
+                print(words, end = " ")
+                continue
+
+    
+
+sentence_splitter('something.txt')
